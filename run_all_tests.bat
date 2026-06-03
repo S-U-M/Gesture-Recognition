2658d7245f7b8d2faa4d6518e782929a1fb69f55
TaylorK2049 <taesubkang2@gmail.com>
2026-06-03 15:43:03 -0700

    test33

---
+++ run_all_tests.bat
@@ -0,0 +1,31 @@
+@echo off
+setlocal
+
+set BASEDIR=%~dp0
+
+set EXE=%BASEDIR%x64\Debug\Project Test.exe
+set IMGDIR=%BASEDIR%
+
+echo Running OK tests...
+"%EXE%" "%IMGDIR%ok1.jpg"
+"%EXE%" "%IMGDIR%ok2.jpg"
+
+echo Running Thumbs Up tests...
+"%EXE%" "%IMGDIR%thumbsup1.jpg"
+"%EXE%" "%IMGDIR%thumbsup2.jpg"
+
+echo Running Pointing tests...
+"%EXE%" "%IMGDIR%pointing1.jpg"
+"%EXE%" "%IMGDIR%pointing2.jpg"
+
+echo Running Peace tests...
+"%EXE%" "%IMGDIR%peace1.jpg"
+"%EXE%" "%IMGDIR%peace2.jpg"
+
+echo Running Stop tests...
+"%EXE%" "%IMGDIR%stop1.jpg"
+"%EXE%" "%IMGDIR%stop2.jpg"
+
+echo.
+echo All tests finished.
+pause
